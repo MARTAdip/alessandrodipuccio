1b52062bd081e444f31041308ebd1e919e560b23
marta <martadipuccio@me.com>
2020-07-20 15:54:11 +0200

    press wip

--- src/pages/press.js
+++ src/pages/press.js
@@ -41,6 +41,27 @@ const Press = ({ intl }) => {
           }
         }
       }
+      alkaline: file(relativePath: { eq: "alkalinejazztrio.jpg" }) {
+        childImageSharp {
+          fixed(quality: 100, width: 300, height: 300) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      floresTribute: file(relativePath: { eq: "florestribute.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      janeGoddall: file(relativePath: { eq: "janegoddall.jpg" }) {
+        childImageSharp {
+          fixed(quality: 100, width: 300, height: 300) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
     }
   `)
 
@@ -75,7 +96,7 @@ const Press = ({ intl }) => {
                 src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/692279068&color=%23304463&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
               ></iframe>
               <div></div>
-              <div>
+              <div className="links">
                 <a
                   href="https://hardwax.com/25602/alessandro-di-puccio/one-instrument-sessions-04/"
                   target="blank"
@@ -124,6 +145,51 @@ const Press = ({ intl }) => {
               </a>
             </div>
             <hr />
+
+
+            <div className="press-inner-wrapper">
+              <h2 className="titolo">
+                Nel giardino di palazzo Casalini, Italia e America a confronto sotto il cielo stellato
+              </h2>
+              <span className="description">
+              <Img fixed={data.alkaline.childImageSharp.fixed} />
+                {/* {intl.formatMessage({ id: "press_description1" })} */}
+              </span>
+              <br />
+             
+              <a
+                href="https://www.radiorovigo.net/nel-giardino-di-palazzo-casalini-italia-e-america-a-confronto-sotto-il-cielo-stellato/"
+                target="blank"
+              >
+                {intl.formatMessage({ id: "press_link1" })}
+              </a>
+            </div>
+            <hr />
+
+            
+            <div className="press-inner-wrapper">
+              <h2 className="titolo">
+                La casa del jazz dove crescono talenti
+              </h2>
+              <span className="description">
+              Si chiama «Silence» e nasce in via degli Artisti nei locali della storica scuola dove si sono formati Stefano Bollani, Piero Borri e Marco Lamioni
+              </span>
+              <br />
+              <br />
+              <AudioPlayer
+                src="https://episodes.castos.com/Controradio-it/191111-Alessandro-Di-Puccio.mp3"
+                onPlay={e => console.log("onPlay")}
+                // other props here
+              />
+              <a
+                href="https://corrierefiorentino.corriere.it/firenze/notizie/cronaca/19_novembre_05/casa-jazz-dove-crescono-talenti-9a7018ea-ffde-11e9-82b5-426614f2da09.shtml"
+                target="blank"
+              >
+                {intl.formatMessage({ id: "press_link1" })}
+              </a>
+            </div>
+
+            <hr />
             <div className="press-inner-wrapper">
               <h2 className="titolo">
                 {intl.formatMessage({ id: "press_title2" })}
@@ -139,15 +205,25 @@ const Press = ({ intl }) => {
               >
                 {intl.formatMessage({ id: "press_link2" })}
               </a>
-              {/* <iframe
-              title="intervista"
-              width="450"
-              height="315"
-              src="https://www.youtube.com/embed/J6tNTZ5ZVpM"
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe> */}
+            </div>
+
+
+            <hr />
+            <div className="press-inner-wrapper">
+              <h2 className="titolo">
+                For Those I Never Knew – Tributo a Luca Flores
+              </h2>
+              <span className="description">
+              Tra gli eventi di maggior spicco va senz’altro annoverato il tributo dedicato a Luca Flores da un quintetto riunito dal vibrafonista Alessandro Di Puccio (suo stretto collaboratore e amico fraterno) sotto la denominazione For Those I Never Knew, una delle più suggestive composizioni del pianista. 
+              </span>
+              <br />
+              <Img fluid={data.floresTribute.childImageSharp.fluid} />
+              <a
+                href="https://www.musicajazz.it/for-those-never-knew-tributo-luca-flores/"
+                target="blank"
+              >
+                {intl.formatMessage({ id: "press_link1" })}
+              </a>
             </div>
 
             <hr />
@@ -187,6 +263,26 @@ const Press = ({ intl }) => {
               ></iframe>
             </div>
             <hr />
+            
+
+            <hr />
+            <div className="press-inner-wrapper">
+              <h2 className="titolo">
+              Les Italiens, concerto per il Jane Goodall Institute
+              </h2>
+              <span className="description">
+              Il trascinante swing mediterraneo de Les Italiens per la prima volta all’Odeon di Firenze, e per una buona causa: martedì 7 febbraio la big-band fondata da Alessandro di Puccio è protagonista di un concerto-evento a favore del Jane Goodall Institute.
+              </span>
+              <br />
+              <Img fixed={data.janeGoddall.childImageSharp.fixed} />
+              <a
+                href="http://www.toscanamusiche.it/les-italiens-concerto-per-il-jane-goodall-institute/"
+                target="blank"
+              >
+                {intl.formatMessage({ id: "press_link1" })}
+              </a>
+            </div>
+
             <div className="press-inner-wrapper">
               <h2 className="titolo">
                 {intl.formatMessage({ id: "press_title5" })}
@@ -205,6 +301,7 @@ const Press = ({ intl }) => {
                 allowFullScreen
               ></iframe>
             </div>
+
           </div>
         </div>
       </Layout>
